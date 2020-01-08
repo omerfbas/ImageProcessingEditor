677df542c2716c1c44be8243d5bb9f53693d47bd
omerfbas <omerbasdev@gmail.com>
2020-01-08 23:00:33 +0300

    Digital resize added

---
+++ WindowsFormsApplication1/DigitalResize.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayisalResize
+{
+    public partial class Form1 : Form
+    {
+        Image img;
+        string[] tip = { ".PNG", ".JPEG", ".JPG" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < tip.Length; i++)
+            {
+                comboBox.Items.Add(tip[i]);
+            }
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textsec.Text = ofd.FileName;
+                img = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                textkydt.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int w = Convert.ToInt32(textw.Text), h = Convert.ToInt32(texth.Text);
+            img = Boyutlandir(img, w, h);
+            ((Button)sender).Enabled = false;
+            MessageBox.Show("Seçilen Resmin Verilen Değerlere Göre Yeniden Boyutlandırılması Tamamlandı.");
+
+        }
+
+        Image Boyutlandir(Image image, int w, int h)
+        {
+            Bitmap bmp = new Bitmap(w, h);
+            Graphics graphic = Graphics.FromImage(bmp);
+            graphic.DrawImage(image, 0, 0, w, h);
+            graphic.Dispose();
+
+            return bmp;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             int nokta = 0, slash = 0;
+             for(int j = textsec.Text.Length - 1; j >= 0; j--)
+                if (textsec.Text[j] == '.')
+                    nokta = j;
+                else if (textsec.Text[j] == '\\')
+                {
+                    slash = j;
+                    break;
+                }
+
+            img.Save(textkydt.Text + "\\" + textsec.Text.Substring(slash + 1, nokta - slash - 1) + "_Boyutlandırılmış" + tip[comboBox.SelectedIndex]);
+            ((Button)sender).Enabled = false;
+            MessageBox.Show("Yeni Resim Başarıyla Kaydedildi.");
+
+
+            /* SaveFileDialog f = new SaveFileDialog();
+
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(f.FileName);
+            }*/
+        }
+    }
+}
---
+++ WindowsFormsApplication1/DigitalResize.designer.cs
@@ -0,0 +1,257 @@
+﻿namespace SayisalResize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textsec = new System.Windows.Forms.TextBox();
+            this.textkydt = new System.Windows.Forms.TextBox();
+            this.textw = new System.Windows.Forms.TextBox();
+            this.texth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resim Seç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(43, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Resmi Kaydet";
+            // 
+            // textsec
+            // 
+            this.textsec.Location = new System.Drawing.Point(276, 28);
+            this.textsec.Margin = new System.Windows.Forms.Padding(4);
+            this.textsec.Name = "textsec";
+            this.textsec.Size = new System.Drawing.Size(443, 22);
+            this.textsec.TabIndex = 2;
+            // 
+            // textkydt
+            // 
+            this.textkydt.Location = new System.Drawing.Point(276, 86);
+            this.textkydt.Margin = new System.Windows.Forms.Padding(4);
+            this.textkydt.Name = "textkydt";
+            this.textkydt.Size = new System.Drawing.Size(443, 22);
+            this.textkydt.TabIndex = 3;
+            // 
+            // textw
+            // 
+            this.textw.Location = new System.Drawing.Point(80, 198);
+            this.textw.Margin = new System.Windows.Forms.Padding(4);
+            this.textw.Name = "textw";
+            this.textw.Size = new System.Drawing.Size(132, 22);
+            this.textw.TabIndex = 4;
+            // 
+            // texth
+            // 
+            this.texth.Location = new System.Drawing.Point(375, 196);
+            this.texth.Margin = new System.Windows.Forms.Padding(4);
+            this.texth.Name = "texth";
+            this.texth.Size = new System.Drawing.Size(132, 22);
+            this.texth.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 201);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(308, 198);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Heigh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(596, 199);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tür";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(656, 194);
+            this.comboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(160, 24);
+            this.comboBox.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ImageKey = "Folder_24.png";
+            this.button2.ImageList = this.imageList1;
+            this.button2.Location = new System.Drawing.Point(749, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 36);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Saved_24.png");
+            this.imageList1.Images.SetKeyName(1, "Folder_24.png");
+            this.imageList1.Images.SetKeyName(2, "View_24.png");
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.ImageKey = "View_24.png";
+            this.button3.ImageList = this.imageList1;
+            this.button3.Location = new System.Drawing.Point(176, 288);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 44);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Boyutlandır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.ImageKey = "Saved_24.png";
+            this.button4.ImageList = this.imageList1;
+            this.button4.Location = new System.Drawing.Point(475, 288);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(225, 44);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ImageKey = "Folder_24.png";
+            this.button1.ImageList = this.imageList1;
+            this.button1.Location = new System.Drawing.Point(749, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 36);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 374);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.texth);
+            this.Controls.Add(this.textw);
+            this.Controls.Add(this.textkydt);
+            this.Controls.Add(this.textsec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Ölçeklendirme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textsec;
+        private System.Windows.Forms.TextBox textkydt;
+        private System.Windows.Forms.TextBox textw;
+        private System.Windows.Forms.TextBox texth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
